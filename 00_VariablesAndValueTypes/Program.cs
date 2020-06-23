78552658fa5cf981323091d8af4ce5ab9a87cd4d
aknight9255 <amanda.knight55@gmail.com>
2020-06-23 14:53:10 -0400

    Add project files.

---
+++ 00_VariablesAndValueTypes/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_VariablesAndValueTypes
+{
+    enum PastryType { Cake, Cupcake, Croissant, Donut, Cookie, Scone,Danish};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bools 
+            bool isDeclared;
+            isDeclared = true;
+
+            bool isDeclaredAndInitialized = false;
+            isDeclaredAndInitialized = true;
+
+            char character = 'a';
+            char symbol = '&';
+            char number = '5';
+            char space = ' ';
+            char specialCharacter = '\n';
+
+            // Whole Numbers 
+            byte byteExample = 255;
+            sbyte sByteExample = -128;
+            short shortexample = 32767;
+            Int16 anotherShortExample = 32000;
+            int intMin = -2147483648;
+            Int32 intmax = 2147483647;
+            long longExample = 9223372036854775807;
+            Int64 longMin = -9223372036854775808;
+
+            int a = 7;
+            int b = -7000;
+
+            byte age = 104;
+
+            // Decimals 
+            float floatExample = 1.045231f;
+            double doubleExample = 1.789053278907036d;
+            decimal decimalExample = 1.2578907289045789789789789787897m;
+            Console.WriteLine(1.2578907289045789789789789787897f);
+            Console.WriteLine(1.2578907289045789789789789787897d);
+            Console.WriteLine(1.2578907289045789789789789787897m);
+
+
+            Console.ReadKey();
+
+            // Enums 
+
+            PastryType myPastry = PastryType.Croissant;
+            PastryType anotherOne = PastryType.Donut;
+
+            //Structs 
+
+            Int32 num = 42;
+            DateTime today = DateTime.Today;
+            DateTime birthday = new DateTime(1805, 11, 24);
+            DateTime test = new DateTime();
+
+        }
+    }
+}
---
+++ 03_IfElseStatements/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_IfElseStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool userIsHungry = true;
+            if (userIsHungry)
+            {
+                Console.WriteLine("Eat Something!");
+            }
+
+            int hourSpentStudying = 1;
+            if(hourSpentStudying < 16)
+            {
+                Console.WriteLine("Are you even trying?");
+            }
+
+            bool choresAreDone = false;
+            if (choresAreDone)
+            {
+                Console.WriteLine("Have fun playing video games");
+            }
+            else
+            {
+                Console.WriteLine("Finish your chores!");
+            }
+
+            Console.WriteLine("How many hours did you sleep?");
+            string input = Console.ReadLine();
+            int totalHours = int.Parse(input);
+
+            if(totalHours >= 8)
+            {
+                Console.WriteLine("Congrats. You are living the dream");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the club");
+                if(totalHours < 3)
+                {
+                    Console.WriteLine("That's rough buddy");
+                }
+            }
+
+
+            Console.WriteLine("How old are you?");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+
+            if(age > 17)
+            {
+                Console.WriteLine("Congrats on being an adult");
+            }
+            else
+            {
+                if(age > 6)
+                {
+                    Console.WriteLine("You are a kiddo");
+                }
+                else if (age > 0)
+                {
+                    Console.WriteLine("You are a wee bebe");
+                }
+                else
+                {
+                    Console.WriteLine("You are not even born");
+                }
+            }
+
+
+            if(age > 65 || age < 18)
+            {
+                Console.WriteLine("You have entered an age greater than 65 OR less than 18.");
+            }
+        }
+    }
+}
