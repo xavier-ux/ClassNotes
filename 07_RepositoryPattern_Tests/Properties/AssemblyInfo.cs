c2a5900531b432afb280ae0082efc423a38b9369
aknight9255 <amanda.knight55@gmail.com>
2020-06-29 11:45:55 -0400

    before lunch notes/Monday

---
+++ 07_RepositoryPattern_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("07_RepositoryPattern_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("07_RepositoryPattern_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7635ad9c-3ac3-4222-bce8-ff2afb210e4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 07_RepositoryPattern_Tests/StreamingContentTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using _07_RepositoryPattern_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _07_RepositoryPattern_Tests
+{
+    [TestClass]
+    public class StreamingContentTests
+    {
+        [TestMethod]
+        public void SetTitle_ShouldSetCorrectString()
+        {
+            StreamingContent content = new StreamingContent();
+            content.Title = "Toy Story";
+
+            string expected = "Toy Story";
+            string actual = content.Title;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
