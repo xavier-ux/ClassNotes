91804c33306af2528d893087b8e9c1b6253fd198
aknight9255 <amanda.knight55@gmail.com>
2020-07-06 11:58:43 -0400

    final before lunch notes

--- 10_StreamingContent_UIRefactorTests/ProgramUITests.cs
+++ 10_StreamingContent_UIRefactorTests/ProgramUITests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using _09_StreamingContent_Console.UI;
+using _10_StreamingContent_UIRefactorTests.UI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace _10_StreamingContent_UIRefactorTests
@@ -7,8 +10,36 @@ namespace _10_StreamingContent_UIRefactorTests
     public class ProgramUITests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetList_OutputShouldContainList()
         {
+            //Arrange 
+            List<string> commandList = new List<string> { "1", "5" };
+            MockConsole console = new MockConsole(commandList);
+            ProgramUI program = new ProgramUI(console);
+
+            //Act
+            program.Run();
+            Console.WriteLine(console.Output);
+
+            //Assert
+            Assert.IsTrue(console.Output.Contains("Avatar the Last Airbender"));
+        }
+
+        [TestMethod]
+        public void AddToList_ShouldSeeItemInList()
+        {
+            //Arrange
+            var customDesc = "This is my custom description";
+            var commandList = new List<string> { "3", "Title", customDesc, "4", "1", "8", "1", "5" };
+            var console = new MockConsole(commandList);
+            var ui = new ProgramUI(console);
+
+            //ACT
+            ui.Run();
+            Console.WriteLine(console.Output);
+
+            //Assert
+            Assert.IsTrue(console.Output.Contains(customDesc));
         }
     }
 }
